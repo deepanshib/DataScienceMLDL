bba5aa1963316e0b3f06b16a8acdd4db536d98d6
Deepanshi Bansal <deepanshibansal@ymail.com>
2018-10-19 20:00:29 +0530

    Add files via upload

---
+++ ann.py
@@ -0,0 +1,88 @@
+# Artificial Neural Network
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# pip install tensorflow
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Part 2 - Now let's make the ANN!
+
+# Importing the Keras libraries and packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Part 3 - Making predictions and evaluating the model
+classifier=Sequential()
+classifier.add(Dense(output_dim=6,input_dim=11,activation='relu',init='uniform'))   #first hidden layer
+classifier.add(Dense(output_dim=6,activation='relu',init='uniform'))   #second hidden layer
+classifier.add(Dense(output_dim=1,activation='sigmoid',init='uniform'))   #second hidden layer
+
+
+
+# working on weights and cost fucntion
+classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+
+
+
+classifier.fit(X_train,y_train,batch_size=10,nb_epoch=50)
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)   #returns probability
+y_pred = (y_pred > 0.5)             #returns true or false
+
+
+# Predicting a single new observation
+"""Predict if the customer with the following informations will leave the bank:
+Geography: France
+Credit Score: 600
+Gender: Male
+Age: 40
+Tenure: 3
+Balance: 60000
+Number of Products: 2
+Has Credit Card: Yes
+Is Active Member: Yes
+Estimated Salary: 50000"""
+new_prediction = classifier.predict(sc.transform(np.array([[0.0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])))
+new_prediction = (new_prediction > 0.5)
+
+
+# Making the Confusion Matrix to validate....... total correct/ total models = accuracy
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
